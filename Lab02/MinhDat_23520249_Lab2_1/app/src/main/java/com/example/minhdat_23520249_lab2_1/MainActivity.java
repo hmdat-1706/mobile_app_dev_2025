2c1ce02e8c0e23ea2f5d76e77122099f80deebe1
hmdat-1706 <hmdat1706@gmail.com>
2025-10-18 01:54:39 +0700

    Lab2_1 Complete

--- Lab02/MinhDat_23520249_Lab2_1/app/src/main/java/com/example/minhdat_23520249_lab2_1/MainActivity.java
+++ Lab02/MinhDat_23520249_Lab2_1/app/src/main/java/com/example/minhdat_23520249_lab2_1/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.minhdat_23520249_lab2_1;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        final String[] arr = {"Teo", "Ty", "Bin", "Bo"}; //Khởi tạo mảng chứa dữ liệu
+        final TextView tvSeletion = findViewById(R.id.tv_selection);
+        ListView lvPerson = findViewById(R.id.lv_person);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> //Tạo và gán Adapter cho ListView
+                (this, android.R.layout.simple_list_item_1, arr);
+        lvPerson.setAdapter(adapter);
+
+        lvPerson.setOnItemClickListener(new AdapterView.OnItemClickListener() { //Thiết lập sự kiện click cho item trong ListView
+            @Override
+            public void onItemClick(AdapterView<?>arg0, View arg1, int arg2, long arg3) {
+                tvSeletion.setText("position: " + arg2 + " ; " + "value= " + arr[arg2]);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
