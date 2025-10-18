889f3521f5e0b5f617a8325eb038bed149de1390
hmdat-1706 <hmdat1706@gmail.com>
2025-10-18 15:58:53 +0700

    Lab2_3 incomplete

---
+++ MinhDat_23520249_Lab2_4/app/src/main/java/com/example/minhdat_23520249_lab2_4/EmployeeAdapter.java
@@ -0,0 +1,58 @@
+package com.example.minhdat_23520249_lab2_4;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class EmployeeAdapter extends ArrayAdapter<Employee> {
+    private Activity context;
+
+    public EmployeeAdapter(Activity context, int layoutID, List<Employee> objects) {
+        super(context, layoutID, objects);
+        this.context = context;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_employee, null, false);
+        }
+
+        Employee employee = getItem(position);
+
+        TextView tvFullName = (TextView) convertView.findViewById(R.id.item_employee_tv_fullName);
+        TextView tvPosition = (TextView) convertView.findViewById(R.id.item_employee_tv_position);
+        ImageView ivManager = (ImageView) convertView.findViewById(R.id.item_employee_iv_manager);
+        LinearLayout llParent = (LinearLayout) convertView.findViewById(R.id.item_employee_ll_parent);
+
+        if (employee.getFullName() != null) {
+            tvFullName.setText(employee.getFullName());
+        } else {
+            tvFullName.setText("");
+        }
+
+        if (employee.isManager()) {
+            ivManager.setVisibility(View.VISIBLE);
+            tvPosition.setVisibility(View.GONE);
+        } else {
+            ivManager.setVisibility(View.GONE);
+            tvPosition.setVisibility(View.VISIBLE);
+            tvPosition.setText(context.getString(R.string.staff));
+        }
+
+        if (position % 2 == 0) {
+            llParent.setBackgroundResource(R.color.white);
+        } else {
+            llParent.setBackgroundResource(R.color.light_blue);
+        }
+
+        return convertView;
+    }
+}
